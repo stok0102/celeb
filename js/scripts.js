e077968da208aa8822fe0a5b7ca53829307fb645
Charlie Baxter and Caleb Stokka <caleb@>
2016-06-02 13:20:19 -0700

    show and hide quiz results and images based on user input, will show error message if a selection is not made

--- js/scripts.js
+++ js/scripts.js
@@ -10,23 +10,27 @@ $(document).ready(function() {
 
     if (score < 9 && ears === "Yes") {
       $("#match").text("Krang from the Ninja Turtles!");
+      $(this).siblings().hide();
       $("#krang").show();
-    };
+    }
 
     else if (score >= 9 && ears === "No") {
       $("#match").text("Tina Fey!");
+      $(this).siblings().hide();
       $("#tina").show();
-    };
+    }
 
     else if (score < 9 && ears === "No") {
       $("#match").text("Gary Busey!");
+      $(this).siblings().hide();
       $("#busey").show();
-    };
+    }
 
-    else if (score <= 9 && ears === "Yes") {
+    else if (score >= 9 && ears === "Yes") {
       $("#match").text("Mrs. Butterworth!");
+      $(this).siblings().hide();
       $("#butterworth").show();
-    };
+    }
 
     else {
       alert("You screwed up.")
